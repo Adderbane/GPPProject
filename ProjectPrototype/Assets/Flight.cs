779a2170d6258e5194e9ad8478526f5296287895
Matt Beer <mab7799@rit.edu>
2017-10-07 16:10:45 -0400

    Threw a simple scene together and got some simple movement working

--- ProjectPrototype/Assets/Flight.cs
+++ ProjectPrototype/Assets/Flight.cs
@@ -2,15 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This Script is responsible for moving the player around in the XY directions.
 public class Flight : MonoBehaviour {
+
+    public float slideRate = 5.0f; //The rate at which the player's ship moves in the XY Plane
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        Rigidbody r = this.GetComponent<Rigidbody>();
+
+        bool left = Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.D);
+        bool up = Input.GetKey(KeyCode.W);
+        bool down = Input.GetKey(KeyCode.S);
+
+        Vector3 steeringForce = Vector3.zero;
+
+        if ((left && this.transform.position.x > -4.0f) || this.transform.position.x > 4.0f)
+        {
+            steeringForce.x -= 1.0f;
+        }
+        if ((right && this.transform.position.x < 4.0f) || this.transform.position.x < -4.0f)
+        {
+            steeringForce.x += 1.0f;
+        }
+        if ((down && this.transform.position.y > -3.0f) || this.transform.position.y > 3.0f)
+        {
+            steeringForce.y -= 1.0f;
+        }
+        if ((up && this.transform.position.y < 3.0f) || this.transform.position.y < -3.0f)
+        {
+            steeringForce.y += 1.0f;
+        }
+
+        steeringForce = steeringForce.normalized * slideRate;
+
+        r.AddForce(steeringForce);
+
 	}
 }
---
+++ ProjectPrototype/Assets/Rails.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is responsible for moving the player and camera along the Z direction, and any looping of scene
+public class Rails : MonoBehaviour {
+
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0.0f, 0.0f, speed * Time.deltaTime));
+    }
+}
